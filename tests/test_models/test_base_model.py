6d21c298d367c251ff25dec250b24572ec279e66
Ossai Cherish Nneka <cherishlyte13@gmail.com>
2023-03-22 07:54:37 -0700

    alx

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -26,7 +26,7 @@ class test_basemodel(unittest.TestCase):
     def tearDown(self):
         try:
             os.remove('file.json')
-        except:
+        except Exception:
             pass
 
     @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') == 'db', "FileStorage")
