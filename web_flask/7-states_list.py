b8e7c9852de52fb4132bc0adf5b7bda8c3737437
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-04-21 06:25:13 -0700

    add 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,18 +8,18 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
-def display_states():
-    """ Render state_list html page to display States created """
-    states = storage.all()
-    return render_template('7-states_list.html', states=states)
-
-
 @app.teardown_appcontext
-def teardown(self):
-    """ Method to remove current SQLAlchemy Session """
+def close_database(exc):
+    """ Closes the database at the end of the request """
     storage.close()
 
 
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """ Render state_list html page to display States created """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
