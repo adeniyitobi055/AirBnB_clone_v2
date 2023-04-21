0c9eb0c1bd0c100a02b8441ebd05867935802c77
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-04-21 05:46:35 -0700

    updates engines

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -43,7 +43,7 @@ class DBStorage:
 
     def all(self, cls=None):
         """ query and return all """
-        if not self.__session:
+        if self.__session:
             self.close()
         objects = {}
         if type(cls) == str:
@@ -81,24 +81,3 @@ class DBStorage:
     def close(self):
         """ Remove current session if active """
         self.__session.remove()
-
-    def get(self, cls, id):
-        """ Retrieve an object """
-        if cls is not None and type(cls) is str and id is not None and\
-                type(id) is str and cls in name2class:
-            cls = name2class[cls]
-            result = self.__session.query(cls).filter(cls.id == id).first()
-            return result
-        else:
-            return None
-
-    def count(self, cls=None):
-        """ Count number of objects in storage """
-        total = 0
-        if type(cls) == str and cls in name2class:
-            cls = name2class[cls]
-            total = self.__session.query(cls).count
-        elif cls is None:
-            for cls in name2class.value():
-                total += self.__session.query(cls).count()
-        return total
