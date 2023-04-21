192c6b446933636753ff84f941e50dd3367bd63c
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-04-21 01:28:52 -0700

    update state.py

--- models/state.py
+++ models/state.py
@@ -3,29 +3,32 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from os import getenv
 import models
-from models.city import City
 import shlex
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", cascade='all, delete',
+                              backref="state")
+    else:
+        name = ""
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = chlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    def __init__(self, *args, **kwargs):
+        """ Initializes state """
+        super().__int__(*args, **kwargs)
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """ fs getter that returns City instances """
+            values_city = models.storage.all("City").values()
+            list_city = []
+            for city in values_city:
+                if (city.state_id == self.id):
+                    list_city.append(city)
+            return list_city
